5b3b87143ac3ae99d9aadef0d3ad82d305b46816
Deivy Sarrazola Alvarez <dsarrazola4@soy.sena.edu.co>
2023-12-07 08:48:46 -0500

    le pusimos los colores y lo decoramos y le arreglamos el color,hicimos el inicio de seccion y registro y le pusimos la base de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main()async {
+  await dotenv.load(fileName:"assets/global/.env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_KEY']!,
+  );
   runApp(const App());
 }
+
